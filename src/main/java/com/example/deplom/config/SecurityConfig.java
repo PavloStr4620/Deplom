692b9cbe0420323ae7c6c67f23b277ce11ea5a70
Pavlo <striyvus800@gmail.com>
2024-06-06 13:30:19 +0300

    add profile admin

--- src/main/java/com/example/deplom/config/SecurityConfig.java
+++ src/main/java/com/example/deplom/config/SecurityConfig.java
@@ -75,4 +75,3 @@ public class SecurityConfig {
         return configuration.getAuthenticationManager();
     }
 }
-
--- src/main/java/com/example/deplom/controllers/AdminController.java
+++ src/main/java/com/example/deplom/controllers/AdminController.java
@@ -1,18 +1,16 @@
 package com.example.deplom.controllers;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping()
-    public String adminPage() {
-        // Ваш код для обробки запитів на /admin
-        return "admin";
+
+
+    @GetMapping("/admin")
+    public String adminProfile(){
+        return "profileAdmin";
     }
 }
-
